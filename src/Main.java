576907ca5862b0947c3f1db376a57501e20764c5
Mahmoud Shukri <88428407+mahmoudshukri@users.noreply.github.com>
2022-06-10 14:44:11 +0200

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s1;
+        String s2;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please enter first statement");
+        s1=scanner.next();
+        System.out.println("Please enter second statement");
+        s2=scanner.next();
+        System.out.println("Results:"+ s1.contains(s2));
+
+    }
+}
